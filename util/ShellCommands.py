22ef1d859eaf1f28d6fd68e62f3e4668f07d9eac
boonyachengdu <boonya@sina.com>
2023-08-31 00:22:11 +0800

    Basic manual learning.

--- util/ShellCommands.py
+++ util/ShellCommands.py
@@ -9,4 +9,25 @@ def run_program_simple():
     os.system("python simple.py")
 
 
+def run_dir_b():
+    os.system("dir /B")
 
+
+def run_type(file):
+    os.system("type {}".format(file))
+
+
+def run_popen_read(file):
+    return os.popen("type {}".format(file)).read()
+
+
+def run_popen_read_lines(file):
+    return os.popen("type {}".format(file)).readlines()
+
+
+def run_popen_read_lines_from_dir_b():
+    return os.popen("dir /B").readlines()
+
+
+def run_popen_output():
+    os.popen("python simple.py").read()
---
+++ util/StartFile.py
@@ -0,0 +1,17 @@
+import os
+
+
+def start_file(file):
+    os.startfile(file)
+
+
+def start_file_html():
+    os.startfile("file.html")
+
+
+def start_file_doc():
+    os.startfile("file.doc")
+
+
+def start_file_py():
+    os.startfile("file.py")
