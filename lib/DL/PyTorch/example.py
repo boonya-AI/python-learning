65651038f9eb0e6120d77f7d9138d9eed2179ef4
boonya <boonya@sina.com>
2023-10-26 17:54:41 +0800

    Run problem resolved!

--- lib/DL/PyTorch/example.py
+++ lib/DL/PyTorch/example.py
@@ -3,7 +3,22 @@
 # https://github.com/pytorch/pytorch
 # 张量计算(如NumPy)具有强大的GPU加速
 # 深度神经网络建立在基于磁带的自动光栅系统上
+# https://zhuanlan.zhihu.com/p/66543791
 import torch
+
+
+def add(tensor3):
+    tensor4 = torch.rand(5, 3)
+    print('tensor3 + tensor4= ', tensor3 + tensor4)
+    print('tensor3 + tensor4= ', torch.add(tensor3, tensor4))
+    # 新声明一个 tensor 变量保存加法操作的结果
+    result = torch.empty(5, 3)
+    torch.add(tensor3, tensor4, out=result)
+    print('add result= ', result)
+    # 直接修改变量
+    tensor3.add_(tensor4)
+    print('tensor3= ', tensor3)
+
 
 if __name__ == "__main__":
     x = torch.rand(5, 3)
